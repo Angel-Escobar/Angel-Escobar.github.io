e0cdddf922fcc064a5cf1643100facdeea2c18ba
AngelEsc-2021 <aescobarv1@miumg.edu.gt>
2023-11-28 20:20:48 -0600

    Cover terminado

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  const BarraOscura = styled.div`
+  background-color: black;
+  `
+  return (
+    <div className='container'>
+      <BarraOscura className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Logo</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse justify-content-end`} id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#" onClick={handleNavCollapse}>Inicio <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" onClick={handleNavCollapse}>Acerca de</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" onClick={handleNavCollapse}>Servicios</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#" onClick={handleNavCollapse}>Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </BarraOscura>
+    </div>
+  );
+};
+
+export default Navbar;
